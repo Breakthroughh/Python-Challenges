7672fb721451ad4a72bd2850f4c2ba6b441602cd
Bryan Lim <112593926+BryanLimLuoTing23@users.noreply.github.com>
2022-12-31 19:47:55 +0800

    Create AS13 - Ratio analysis.py

---
+++ AS13 - Ratio analysis.py
@@ -0,0 +1,8 @@
+grossprofitmargin = 0
+
+grossprofit = int(input("To calculate gross profit margin, please start by entering gross profit: "))
+totalsales = int(input("Enter total value of sales: "))
+
+grossprofitmargin = grossprofit / totalsales *100
+
+print("Gross profit margin = "+str(grossprofitmargin)+"%")
